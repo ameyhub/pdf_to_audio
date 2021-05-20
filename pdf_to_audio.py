8b25d3350f8f82d99a02e5fe59720f47be19cda7
Amey Didolkar <ameydidolkar88@gmail.com>
2021-05-20 08:31:54 +0530

    first commit

---
+++ pdf_to_audio.py
@@ -0,0 +1,19 @@
+"""
+
+        @author = Amey Didolkar
+
+"""
+
+
+import pyttsx3
+import PyPDF2
+book = open('abhishek.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(0, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
